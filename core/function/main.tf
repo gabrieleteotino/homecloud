447911da4ed13dc066163ab32312c7a6b6d53485
Gabriele Teotino <gabriele@teosoft.it>
2022-02-13 00:48:43 +0000

    Use dedicated storage account for function

--- core/function/main.tf
+++ core/function/main.tf
@@ -31,9 +31,31 @@ resource "azurerm_app_service_plan" "funz" {
   }
 }
 
-resource "azurerm_management_lock" "appi_lock" {
-  name       = "resource-appi-funz-lock"
+resource "azurerm_management_lock" "plan_lock" {
+  name       = "resource-plan-funz-lock"
   scope      = azurerm_app_service_plan.funz.id
+  lock_level = "CanNotDelete"
+  notes      = "Locked. This is a core component."
+}
+
+resource "azurerm_storage_account" "funz" {
+  name                      = module.naming.storage_account.name_unique
+  resource_group_name       = azurerm_resource_group.funz.name
+  location                  = azurerm_resource_group.funz.location
+  account_kind              = "StorageV2"
+  account_tier              = "Standard"
+  account_replication_type  = "LRS"
+  access_tier               = "Hot"
+  min_tls_version           = "TLS1_2"
+  shared_access_key_enabled = true
+  blob_properties {
+    versioning_enabled = false
+  }
+}
+
+resource "azurerm_management_lock" "st_lock" {
+  name       = "resource-st-funz-lock"
+  scope      = azurerm_storage_account.funz.id
   lock_level = "CanNotDelete"
   notes      = "Locked. This is a core component."
 }
@@ -43,12 +65,13 @@ resource "azurerm_function_app" "funz" {
   location                   = local.location
   resource_group_name        = azurerm_resource_group.funz.name
   app_service_plan_id        = azurerm_app_service_plan.funz.id
-  storage_account_name       = data.azurerm_storage_account.core.name
-  storage_account_access_key = data.azurerm_storage_account.core.primary_access_key
+  storage_account_name       = azurerm_storage_account.funz.name
+  storage_account_access_key = azurerm_storage_account.funz.primary_access_key
   https_only                 = true
   version                    = "~4"
   app_settings = {
     FUNCTIONS_WORKER_RUNTIME = "dotnet",
+    CoreStorageAccount       = data.azurerm_storage_account.core.name
   }
   site_config {
     always_on     = false
@@ -57,7 +80,7 @@ resource "azurerm_function_app" "funz" {
 }
 
 resource "azurerm_management_lock" "func_lock" {
-  name       = "resource-funz-lock"
+  name       = "resource-func-funz-lock"
   scope      = azurerm_function_app.funz.id
   lock_level = "CanNotDelete"
   notes      = "Locked. This is a core component."
